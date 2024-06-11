5169e41578f338ac707a9d7b77bb9ff2615b4cb0
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2024-06-11 23:33:59 +0530

    queue done by just watching theroy part

---
+++ index.js
@@ -0,0 +1,38 @@
+class Queue {
+  constructor() {
+    this.arr = [];
+  }
+
+  add(value) {
+    this.arr.push(value);
+  }
+
+  show() {
+    let printQueueValue = '';
+    this.arr.map((item) => (printQueueValue += item));
+
+    return printQueueValue;
+  }
+
+  remove() {
+    for (let index = 0; index >= this.arr.length; index++) {
+      this.arr[index] = this.arr[index + 1];
+    }
+  }
+
+  removeWithShift() {
+    this.arr.shift();
+  }
+}
+
+const QueueCalling = new Queue();
+
+QueueCalling.add(4);
+QueueCalling.add(3);
+QueueCalling.add(5);
+QueueCalling.add(7);
+QueueCalling.add(8);
+
+console.log(QueueCalling.show());
+QueueCalling.remove();
+console.log(QueueCalling.show());
